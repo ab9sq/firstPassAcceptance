c8964cfad6a0be6e609d59a24497956363606f60
Nick Lauerman <nicholas.lauerman@gmail.com>
2018-02-05 14:41:41 -0600

    Updated FirstPassAccetance.R functions to filter for Month. Removed Month for functions that were not using it. Removed lubridate package from being required in the clean.FPA function. as it is not used. Fixed as.Date formate in the Clean functin

--- scripts/FirstPassAcceptance.R
+++ scripts/FirstPassAcceptance.R
@@ -1,4 +1,4 @@
-get.FPA <- function(file = "./data/First Pass Acceptance.csv", month = NULL){
+get.FPA <- function(file = "./data/First Pass Acceptance.csv"){
      FPA <- read.csv( file = file,
                       stringsAsFactors = FALSE,
                       col.names = c("sqa",
@@ -13,27 +13,49 @@ get.FPA <- function(file = "./data/First Pass Acceptance.csv", month = NULL){
                                     "comments"))
 }
 
-clean.FPA <- function(FPA, month = NULL){
-     require(lubridate)
+clean.FPA <- function(FPA){
+     #require(lubridate)
      FPA$sqa <- as.factor(FPA$sqa)
      FPA$application <- as.factor(FPA$application)
      FPA$deliverable <- as.factor(FPA$deliverable)
      FPA$status <- as.factor(FPA$status)
      FPA$reason <- as.factor(FPA$reason)
      levels(FPA$reason)[1] <- NA
-     FPA$date <- as.Date(FPA$date, format = "%d-%B-%y")
+     FPA$date <- as.Date(FPA$date, format = "%d-%B-%Y")
      return(FPA)
 }
 
-process <- function(FPA){
+process <- function(FPA, Month = NULL){
      require(lubridate)
-     FPA$month <- month(FPA$date, label = TRUE)
-     FPA$year <- year(FPA$date)
+     FPA$month <- lubridate::month(FPA$date, label = TRUE)
+     FPA$year <- lubridate::year(FPA$date)
      FPA$UID <- paste(FPA$project,
                       FPA$deliverable,
                       FPA$docID,
                       FPA$version,
                       sep = "-")
+     if(!(is.null(Month))){
+          # print("Process for not NULL")
+          if(Month %in% levels(FPA$month)){
+               FPA <- subset(FPA,
+                             subset =  month == Month)
+          }else{
+               #print("Process for invlad month")
+               stop(paste("Invalid month. Month must be one of the following: NULL,",
+                          "Jan,",
+                          "Feb,",
+                          "Mar,",
+                          "Apr,",
+                          "May,",
+                          "Jun,",
+                          "Jul,",
+                          "Aug,",
+                          "Sep,",
+                          "Oct,",
+                          "Nov or",
+                          "Dec."))
+          }
+     }
      return(FPA)
 }
 
