f34f2f0a6c75efdb630bc0aaeccb72dc55240a08
Nick Lauerman <nicholas.lauerman@gmail.com>
2018-12-10 14:47:03 -0600

    building trending items

--- scripts/Trending_Build.R
+++ scripts/Trending_Build.R
@@ -2,16 +2,27 @@ buildTrending <- function(Month = NULL,
                           Year = NULL,
                           file = "./data/First Pass Acceptance.csv",
                           previous = "./data/FPA.RData"){
+     # checking inputs
+     if(is.null(Month) | is.na(Month)){stop("Month value must be specified")}
+     if(is.null(Year) | is.na(Year)){stop("Year value must be specified")}
+     if(is.null(file) | is.na(file)){stop("file value must be specified")}
+     if(anyNA(previous)){stop("previous value must be specified or NULL")}
+     if(!is.null(previous)){
+          load(file = previous)
+     } else{
+          FPA <- NULL
+          previous <- "./data/FPA.RData"
+     }
 
-     # Load functions
+     # Load required  functions
      source(file = "./scripts/Monthly_Stats.R")
-
      # Get months data
      output <- getResults(Month = Month,
                           Year  = Year,
                           file  = file)
-     load(file = previous)
      FPA <- rbind(FPA, output)
+
      save(FPA, file = previous)
+     # rm(getResults) need to figure out
      return(FPA)
 }
---
+++ scripts/scratch.R
@@ -0,0 +1,3 @@
+load(file = "./data/FPA.RData")
+rm(FPA)
+buildTrending(Month = "Nov", Year = 2018)
