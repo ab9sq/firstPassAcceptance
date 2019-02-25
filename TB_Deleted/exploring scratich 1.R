761b998e978f568cd6640971fbd83201a0bec2f9
Nick Lauerman <nicholas.lauerman@gmail.com>
2019-02-25 12:49:46 -0600

    Update LaATeX for easier processing/error reduction. cleaned up old R file

--- TB_Deleted/exploring scratich 1.R
+++
@@ -1,5 +0,0 @@
-length(
-     unique(
-          subset(
-               subset(FPA, subset = (status == "A" | status == "A-FP")),
-               subset = deliverable == "Software Change Request")$project))
