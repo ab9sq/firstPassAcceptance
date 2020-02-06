99b9474f61fb612c2b4c8eb60f037973209df35b
Nick Lauerman <nicholas.lauerman@gmail.com>
2020-02-06 11:06:40 -0600

    Updated for further processing

--- special.R
+++ special.R
@@ -1,28 +1,50 @@
-Month <- "Dec"
-Year <- 2019
-DivFile <- "./data/Div First Pass Acceptance.csv"
+Month <- "Jan"
+Year <- 2020
 
 source("./scripts/FirstPassAcceptance.R")
 require(lubridate)
 require(tidyverse)
-hold <- getFPA(file = DivFile)
-hold <- cleanFPA(hold)
-hold <- process(hold)
+Div <- getFPA(file = "./data/Div First Pass Acceptance.csv")
+Div <- cleanFPA(Div)
+Div <- process(Div)
+Div$Site <- "LC/Div"
 
-temp <- hold %>%
+Dallas <- getFPA(file = "./data/Dallas First Pass Acceptance.csv")
+Dallas <- cleanFPA(Dallas)
+Dallas <- process(Dallas)
+Dallas$Site <- "Dallas"
+
+Wiesbaden <- getFPA(file = "./data/Wiesbaden First Pass Acceptance.csv")
+Wiesbaden <- cleanFPA(Wiesbaden)
+Wiesbaden <- process(Wiesbaden)
+Wiesbaden$Site <- "Wiesbaden"
+
+hold <- rbind(Div,
+              Dallas,
+              Wiesbaden)
+
+Work <- hold %>%
      group_by(year, month, sqa) %>%
      count()
 
+Effort <- hold %>%
+        group_by(year, month, sqa) %>%
+        count() %>%
+        filter((year == 2020 & month == "Jan"))
+Effort
 
-
-temp$date <- as.Date(paste("1",
-                           temp$month,
-                           temp$year,
+Work$date <- as.Date(paste("1",
+                           Work$month,
+                           Work$year,
                            sep = "-"),
                      format = "%d-%b-%Y")
+Work <- Work %>%
+        filter(date <= as.Date("31-Jan-2020",
+                               format = "%d-%b-%Y"))
+
+holdplot <- ggplot(data = Work, aes(x= date, y=n))
 
 
-holdplot <- ggplot(data = temp, aes(x= date, y=n))
 holdplot+geom_line(aes(group = sqa, color = sqa))
 ggsave(filename = "interesting.jpg",
        width = 6,
