0f45a245ee561221318c9a75cc112feb0fd1c382
Lauerman <nick.lauerman@abbott.com>
2021-02-15 16:51:34 -0600

    Dec 2020 Results update

--- scripts/Liz Information.R
+++ scripts/Liz Information.R
@@ -2,7 +2,7 @@ source("./scripts/FirstPassAcceptance.R")
 require(lubridate)
 require(tidyverse)
 
-Month <- "Nov"
+Month <- "Dec"
 Year <- 2020
  EndOfPeriod <- as.Date("31-Jul-2020",
                        format = "%d-%b-%Y")
